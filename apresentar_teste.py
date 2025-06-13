a712cab7199e44f763c1ca7bfb36caa21c71535f
clarobrshare <clarobrshare@gmail.com>
2025-06-13 11:04:13 -0300

    Update apresentar_teste.py

--- apresentar_teste.py
+++ apresentar_teste.py
@@ -112,7 +112,7 @@ def plot_absolute_ranks(data):
     
     # Adiciona uma única legenda acima dos subplots
     handles, labels = axes[0].get_legend_handles_labels()
-    fig.legend(handles, labels, title="Rank", bbox_to_anchor=(0.5, 1.1), loc='upper center', ncol=3, fontsize='small')
+    fig.legend(handles, labels, title="Rank", bbox_to_anchor=(0.5, 1.15), loc='upper center', ncol=4, fontsize='small')
     
     plt.tight_layout(rect=[0, 0, 1, 0.95])  # Ajusta o layout para dar espaço à legenda
     st.pyplot(fig)
@@ -150,7 +150,7 @@ def plot_percentage_ranks(data):
 
     # Adiciona uma única legenda acima dos subplots
     handles, labels = axes[0].get_legend_handles_labels()
-    fig.legend(handles, labels, title="Rank", bbox_to_anchor=(0.5, 1.1), loc='upper center', ncol=3, fontsize='small')
+    fig.legend(handles, labels, title="Rank", bbox_to_anchor=(0.5, 1.15), loc='upper center', ncol=4, fontsize='small')
 
     plt.tight_layout(rect=[0, 0, 1, 0.95])  # Ajusta o layout para dar espaço à legenda
     st.pyplot(fig)
